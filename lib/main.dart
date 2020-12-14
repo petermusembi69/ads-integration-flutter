91f8d2252cac795ba1ed15c0a444f0bb0ce7b510
petermusembi69 <nyongesamusembi@gmail.com>
2020-12-14 15:08:10 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,23 @@
+import 'package:ads_integration_flutter/admob/googleAdmob.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:admob_flutter/admob_flutter.dart';
+import './services/admob_services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // debugShowMaterialGrid: true,
+      // debugShowCheckedModeBanner: true,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -34,45 +27,62 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // initializes our admob service
+  final ams = AdMobServices();
+
+  @override
+  void initState() {
+    super.initState();
+    // initializes the admob package
+    var appID = ams.getAdmobAppId();
+    Admob.initialize(testDeviceIds: [appID]);
+  }
+
+  int _currentIndex = 0;
+
+  List<BottomNavigationBarItem> _items = [
+    BottomNavigationBarItem(
+      icon: Icon(Icons.home),
+      label: 'Home',
+      backgroundColor: Colors.black,
+    ),
+    BottomNavigationBarItem(
+      icon: FaIcon(FontAwesomeIcons.google),
+      label: 'Google Admob',
+      backgroundColor: Colors.black,
+    ),
+    BottomNavigationBarItem(
+      icon: FaIcon(FontAwesomeIcons.facebook),
+      label: 'Facebook Ads',
+      backgroundColor: Colors.black,
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.games),
+      label: 'Unity Ads',
+      backgroundColor: Colors.black,
+    ),
+  ];
+
+  // AdmobBanner(
+  //         adSize: AdmobBannerSize.FULL_BANNER,
+  //         adUnitId: ams.getBannerAddId(),
+  //         onBannerCreated: _onBanner,
+  //       )
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    List screens = [
+      Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextLiquidFill(
-              text: 'ADS APP',
+              text: 'ADS APP Flutter',
               waveColor: Colors.blueAccent,
               boxBackgroundColor: Colors.redAccent,
               textStyle: TextStyle(
@@ -83,6 +93,195 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-    );
+      Align(
+        alignment: Alignment.center,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(children: [
+                Center(
+                  child: Text('Banner Add'),
+                ),
+                SizedBox(height: 10),
+                AdmobBanner(
+                  adSize: AdmobBannerSize.FULL_BANNER,
+                  adUnitId: ams.getBannerAddId(),
+                  onBannerCreated: _onBanner,
+                ),
+              ]),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (BuildContext context) {
+                    return AdmobFullPage();
+                  }));
+                },
+                child: Container(
+                  width: 200,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                      color: Colors.blue[200]),
+                  padding: EdgeInsets.all(5),
+                  child: Center(
+                    child: Text('Full Page Add'),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: 200,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                      color: Colors.green),
+                  padding: EdgeInsets.all(5),
+                  child: Center(
+                    child: Text('Reward Ad'),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: 200,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                      color: Colors.orange),
+                  padding: EdgeInsets.all(5),
+                  child: Center(
+                    child: Text('Layout Ad'),
+                  ),
+                ),
+              ),
+            ]),
+      ),
+      Align(
+        alignment: Alignment.center,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text('Banner Add'),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: 200,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                      color: Colors.blue[200]),
+                  padding: EdgeInsets.all(5),
+                  child: Center(
+                    child: Text('Full Page Add'),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: 200,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                      color: Colors.green),
+                  padding: EdgeInsets.all(5),
+                  child: Center(
+                    child: Text('Reward Ad'),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: 200,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                      color: Colors.orange),
+                  padding: EdgeInsets.all(5),
+                  child: Center(
+                    child: Text('Layout Ad'),
+                  ),
+                ),
+              ),
+            ]),
+      ),
+      Align(
+        alignment: Alignment.center,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text('Banner Add'),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: 200,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                      color: Colors.blue[200]),
+                  padding: EdgeInsets.all(5),
+                  child: Center(
+                    child: Text('Full Page Add'),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: 200,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                      color: Colors.green),
+                  padding: EdgeInsets.all(5),
+                  child: Center(
+                    child: Text('Reward Ad'),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: 200,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                      color: Colors.orange),
+                  padding: EdgeInsets.all(5),
+                  child: Center(
+                    child: Text('Layout Ad'),
+                  ),
+                ),
+              ),
+            ]),
+      ),
+    ];
+
+    return Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          items: _items,
+          currentIndex: _currentIndex,
+          backgroundColor: Colors.black,
+          selectedItemColor: Colors.blue,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+        ),
+        appBar: AppBar(
+          title: Text('Adds App'),
+        ),
+        body: screens[_currentIndex]);
+  }
+
+  void _onBanner(AdmobBannerController controller) {
+    print(1111122111);
   }
 }
