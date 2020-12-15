db70b5a708349eab10d5ea6ade5b437b09ebc1ac
Peter Musembi <nyongesamusembi@gmail.com>
2020-12-15 11:10:50 +0300

    Update admob_services.dart

--- lib/services/admob_services.dart
+++ lib/services/admob_services.dart
@@ -3,21 +3,21 @@ import 'dart:io';
 class AdMobServices {
   String getAdmobAppId() {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-4117522516334526~2284903907';
+      return 'your-key';
     }
     return null;
   }
 
   String getBannerAddId() {
      if (Platform.isAndroid) {
-      return 'ca-app-pub-4117522516334526/3110900414';
+      return 'your-key';
     }
     return null;
   }
 
   String getFullPageAddId() {
      if (Platform.isAndroid) {
-      return 'ca-app-pub-4117522516334526/4455911297';
+      return 'your-key';
     }
     return null;
   }
