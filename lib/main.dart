5c4517c968dfe602e2866e6320750063dd61eb0f
petermusembi69 <nyongesamusembi@gmail.com>
2020-12-23 13:17:04 +0300

    added app  open ad

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,31 @@
 import 'package:ads_integration_flutter/screensPage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_admob_app_open/flutter_admob_app_open.dart';
 
+void main() async {
+  // app open add implementation begins here
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
+  /// Replace your admob app ID
+  final admobAppId = FlutterAdmobAppOpen.testAppId;
+
+  /// Replace your admob app open ad unit id
+  final appAppOpenAdUnitId = FlutterAdmobAppOpen.testAppOpenAdId;
+
+  MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
+    keywords: <String>['flutterio', 'beautiful apps'],
+    contentUrl: 'https://flutter.io',
+    childDirected: false,
+    testDevices: <String>[], // Android emulators are considered test devices
+  );
+
+  await FlutterAdmobAppOpen.instance.initialize(
+    appId: admobAppId,
+    appAppOpenAdUnitId: appAppOpenAdUnitId,
+    targetingInfo: targetingInfo,
+  );
+  // app open add implementation ends here
+
   runApp(MyApp());
 }
 
