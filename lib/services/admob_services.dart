80fcae8e388a6f6df1627730c5a8607f659cafde
petermusembi69 <nyongesamusembi@gmail.com>
2020-12-23 11:04:04 +0300

    added test ad units ids

--- lib/services/admob_services.dart
+++ lib/services/admob_services.dart
@@ -1,23 +1,42 @@
 import 'dart:io';
 
 class AdMobServices {
+// Class implements helper methods that provive unit IDs and app ID
+
+  // Method provides app ID
   String getAdmobAppId() {
     if (Platform.isAndroid) {
-      return 'your-key';
+      return 'ca-app-pub-4117522516334526~2284903907';
     }
     return null;
   }
 
+  // Method provides banner ID
   String getBannerAddId() {
      if (Platform.isAndroid) {
-      return 'your-key';
+      return 'ca-app-pub-3940256099942544/6300978111';
     }
     return null;
   }
 
+  // Method provides interID
   String getFullPageAddId() {
      if (Platform.isAndroid) {
-      return 'your-key';
+      return 'ca-app-pub-3940256099942544/1033173712';
+    }
+    return null;
+  }
+
+  String getRewardAddId() {
+     if (Platform.isAndroid) {
+      return 'ca-app-pub-3940256099942544/5224354917';
+    }
+    return null;
+  }
+
+  String getLayoutAddId() {
+     if (Platform.isAndroid) {
+      return 'ca-app-pub-4117522516334526/7380725010';
     }
     return null;
   }
