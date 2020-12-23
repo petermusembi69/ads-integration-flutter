f13302da794ea61b43c006032f2c79c62df993b4
petermusembi69 <nyongesamusembi@gmail.com>
2020-12-23 11:07:34 +0300

    added comments

--- lib/services/admob_services.dart
+++ lib/services/admob_services.dart
@@ -11,7 +11,7 @@ class AdMobServices {
     return null;
   }
 
-  // Method provides banner ID
+  // Method provides banner unit ID
   String getBannerAddId() {
      if (Platform.isAndroid) {
       return 'ca-app-pub-3940256099942544/6300978111';
@@ -19,14 +19,15 @@ class AdMobServices {
     return null;
   }
 
-  // Method provides interID
+  // Method provides interstial unit ID
   String getFullPageAddId() {
      if (Platform.isAndroid) {
       return 'ca-app-pub-3940256099942544/1033173712';
     }
     return null;
   }
-
+  
+  // Method provides reward unit ID
   String getRewardAddId() {
      if (Platform.isAndroid) {
       return 'ca-app-pub-3940256099942544/5224354917';
@@ -34,6 +35,7 @@ class AdMobServices {
     return null;
   }
 
+  // Method provides lyout unit ID
   String getLayoutAddId() {
      if (Platform.isAndroid) {
       return 'ca-app-pub-4117522516334526/7380725010';
